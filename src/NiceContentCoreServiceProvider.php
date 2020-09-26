cf3b836438ec1ec7206df0184afbdc59fde6b9d1
Malykhin Sergey <malikin.sergey.1988@gmail.com>
2020-09-26 20:00:41 +0300

    initial

---
+++ src/NiceContentCoreServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NiceContent\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class NiceContentCoreServiceProvider
+ * @package Mlsg\AcmsCore
+ */
+class NiceContentCoreServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
