982dc4ba6d2752dce2076da487e20320a2617d2c
Malykhin Sergey <malikin.sergey.1988@gmail.com>
2020-12-29 20:42:23 +0300

    fix image size

--- resources/views/types/image/show.blade.php
+++ resources/views/types/image/show.blade.php
@@ -1,5 +1,5 @@
 @if($value)
-    <img src="{{$value}}" alt="" class="img-fluid">
+    <img src="{{$value}}" alt="" class="img-fluid" style="max-width: 300px">
 @else
     <p class="text-muted">Изображение не загружено</p>
 @endif
