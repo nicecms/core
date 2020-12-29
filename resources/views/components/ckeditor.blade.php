5da4c9eec908abd30a6990ca9c6024f79b101cb9
Malykhin Sergey <malikin.sergey.1988@gmail.com>
2020-12-29 20:28:24 +0300

    fix rich 1

--- resources/views/components/ckeditor.blade.php
+++ resources/views/components/ckeditor.blade.php
@@ -1,79 +1,87 @@
 <script src="/js/ckeditor/ckeditor.js"></script>
-<script>ClassicEditor
-        .create( document.querySelector( '.editor' ), {
+<script>
 
-            simpleUpload: {
-                // The URL that the images are uploaded to.
-                uploadUrl: '{{config('ckeditor-upload.upload_url')}}',
+    $('.editor').each(function(){
 
-                // Enable the XMLHttpRequest.withCredentials property.
-                withCredentials: true,
+        ClassicEditor
+            .create( this, {
 
-                // Headers sent along with the XMLHttpRequest to the upload server.
-                headers: {
-                    'X-CSRF-TOKEN': '{{csrf_token()}}',
-                }
-            },
-            toolbar: {
-                items: [
-                    'heading',
-                    '|',
-                    'fontBackgroundColor',
-                    'fontColor',
-                    'fontSize',
-                    '|',
-                    'bold',
-                    'italic',
-                    'underline',
-                    'link',
-                    'bulletedList',
-                    'numberedList',
-                    'subscript',
-                    'superscript',
-                    '|',
-                    'alignment',
-                    'indent',
-                    'outdent',
-                    '|',
-                    'imageUpload',
-                    'blockQuote',
-                    'insertTable',
-                    'mediaEmbed',
-                    'htmlEmbed',
-                    '|',
-                    'removeFormat',
-                    '|',
-                    'undo',
-                    'redo'
-                ]
-            },
-            language: 'ru',
-            image: {
-                toolbar: [
-                    'imageTextAlternative',
-                    'imageStyle:full',
-                    'imageStyle:side',
-                    'linkImage'
-                ]
-            },
-            table: {
-                contentToolbar: [
-                    'tableColumn',
-                    'tableRow',
-                    'mergeTableCells',
-                    'tableCellProperties',
-                    'tableProperties'
-                ]
-            },
-            licenseKey: '',
+                simpleUpload: {
+                    // The URL that the images are uploaded to.
+                    uploadUrl: '{{config('ckeditor-upload.upload_url')}}',
 
-        } )
-        .then( editor => {
-            window.editor = editor;
+                    // Enable the XMLHttpRequest.withCredentials property.
+                    withCredentials: true,
 
-        } )
-        .catch( error => {
-            console.error( error );
-        } );
+                    // Headers sent along with the XMLHttpRequest to the upload server.
+                    headers: {
+                        'X-CSRF-TOKEN': '{{csrf_token()}}',
+                    }
+                },
+                toolbar: {
+                    items: [
+                        'heading',
+                        '|',
+                        'fontBackgroundColor',
+                        'fontColor',
+                        'fontSize',
+                        '|',
+                        'bold',
+                        'italic',
+                        'underline',
+                        'link',
+                        'bulletedList',
+                        'numberedList',
+                        'subscript',
+                        'superscript',
+                        '|',
+                        'alignment',
+                        'indent',
+                        'outdent',
+                        '|',
+                        'imageUpload',
+                        'blockQuote',
+                        'insertTable',
+                        'mediaEmbed',
+                        'htmlEmbed',
+                        '|',
+                        'removeFormat',
+                        '|',
+                        'undo',
+                        'redo'
+                    ]
+                },
+                language: 'ru',
+                image: {
+                    toolbar: [
+                        'imageTextAlternative',
+                        'imageStyle:full',
+                        'imageStyle:side',
+                        'linkImage'
+                    ]
+                },
+                table: {
+                    contentToolbar: [
+                        'tableColumn',
+                        'tableRow',
+                        'mergeTableCells',
+                        'tableCellProperties',
+                        'tableProperties'
+                    ]
+                },
+                licenseKey: '',
+
+            } )
+            .then( editor => {
+                // window.editor = editor;
+
+            } )
+            .catch( error => {
+                console.error( error );
+            } );
+
+    })
+
+
 </script>
 
